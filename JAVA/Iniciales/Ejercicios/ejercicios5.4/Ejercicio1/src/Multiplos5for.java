90c0a70fc4fb2745a61ace4b37824dc5e8b38ffe
jony12004 <alexsapu22@gmail.com>
2024-10-29 00:48:09 +0100

    Ejercio1 terminado

---
+++ JAVA/Iniciales/Ejercicios/ejercicios5.4/Ejercicio1/src/Multiplos5for.java
@@ -0,0 +1,15 @@
+
+public class Multiplos5for {
+
+    public static void main(String[] args) {
+    Multiplos5for.multiplo5();
+    }
+
+    public static void multiplo5(){
+        for(int i = 0; i <= 100; i += 5){
+            System.out.println(i);
+        }
+    }
+
+
+}
