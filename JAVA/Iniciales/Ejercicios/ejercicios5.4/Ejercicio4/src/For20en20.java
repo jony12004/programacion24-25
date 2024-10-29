0db131ccc262794d1a71ec68f4018eab9e1011ab
jony12004 <alexsapu22@gmail.com>
2024-10-29 01:06:29 +0100

    Ejercicioo4 terminado

---
+++ JAVA/Iniciales/Ejercicios/ejercicios5.4/Ejercicio4/src/For20en20.java
@@ -0,0 +1,12 @@
+public class For20en20 {
+    public static void main(String[] args) {
+        System.out.println("*** Ejercicio 4 contar de 20 en 20 for ***");
+        For20en20.contar20();
+    }
+    public static void contar20(){
+        for (int i = 360; i>= 160; i-=20){
+            System.out.println(i);
+        }
+
+    }
+}
